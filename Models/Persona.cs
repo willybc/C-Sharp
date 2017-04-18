a306b2bda9e695fe70b54e5cffad5d4664781873
Willy Bardales <willybc06@gmail.com>
2017-04-18 19:46:51 -0300

    Introduccion a la programacion orientada a objetos

---
+++ Models/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace institucion.Models
+{
+    class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public short Edad { get; set; }
+        public int Teléfono { get; set; }
+        public string NombreCompleto
+        {
+            get {
+                return Nombre + " " + Apellido;
+                }
+        }
+
+
+
+    }
+
+  
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using institucion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace institucion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GESTION DE INSTITUCION");
+
+            var alumno1 = new Alumno()
+            {
+              Id=1,
+              Nombre="Willy",
+              Apellido="Bardales",
+              Edad=20,
+              Teléfono=1166861931,
+              Email="willybc06@gmail.com"
+            };
+
+            var persona1 = new Persona()
+            {
+                Id = 1,
+                Nombre = "Juan"
+                Apellido = "Perez",
+                Edad = 25,
+                Teléfono = 55555555,
+                
+            }
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
